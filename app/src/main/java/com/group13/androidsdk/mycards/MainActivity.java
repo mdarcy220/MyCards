e511d09047ca28296a7be2aff6d9ea4b1bd11cdd
Mike D'Arcy <m.m.darcy@csuohio.edu>
2017-04-23 13:23:21 -0400

    Add review button functionality to Main Activity

--- app/src/main/java/com/group13/androidsdk/mycards/MainActivity.java
+++ app/src/main/java/com/group13/androidsdk/mycards/MainActivity.java
@@ -16,8 +16,10 @@
 
 package com.group13.androidsdk.mycards;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartReviewBtnClick(View v) {
+        if(v.getId() != R.id.btnStartReview) {
+            return;
+        }
+        Intent intent = new Intent(this, ReviewFrontActivity.class);
+        startActivity(intent);
+
     }
 }
